2441b8c55706cdede14a3f4d0c757fcd15b218fd
itsjahidhasan <mdjahidhasanf92@gmail.com>
2023-01-11 02:01:13 +0600

    Project bootstarped

---
+++ pages/student/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Container } from "@mui/material";
+
+export default function StudentHome() {
+  return (
+    <Container>
+      <Box>
+        <h1>Hello jahid</h1>
+      </Box>
+    </Container>
+  );
+}
